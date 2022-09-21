0d64ea64f53787795729ce5521f3ac8ba372255c
Doheera-kosi <kupourevans99@gmail.com>
2022-09-21 08:43:56 +0000

    Update project files

--- script.js
+++ script.js
@@ -1,47 +1,50 @@
-/* eslint-disable quotes */
-/* eslint-disable linebreak-style */
 const data = [
   {
     id: '1',
     img: './assets/images/speaker_01.png',
     name: 'Yochai BenKler',
-    position: 'Lorem, ipsum dolor sit amet consectetur.',
-    ach: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+    position: 'Berkman Professor of Entrepreneurial Legal Studies at Havard Law School',
+    ach: 'Benkler known for his published book, the wealth of Network.',
   },
+
   {
     id: '2',
     img: './assets/images/speaker_02.png',
-    name: 'Yochai BenKler',
-    position: 'Lorem, ipsum dolor sit amet consectetur.',
-    ach: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+    name: 'Kilnam Chon',
+    position: 'Kilnam Chon is an internet enthusiast who has been experienced in the IT industry for years.',
+    ach: 'Kilnam Chon is an internet enthusiast who has been experienced in the IT industry for years.',
   },
+
   {
     id: '3',
     img: './assets/images/speaker_03.png',
-    name: 'Yochai BenKler',
-    position: 'Lorem, ipsum dolor sit amet consectetur.',
-    ach: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+    name: 'Julia Leda',
+    position: 'President of Young Pirates of Europe.',
+    ach: 'Leda who supports and encourages young youths in digital and tech field.',
   },
+
   {
     id: '4',
     img: './assets/images/speaker_04.png',
-    name: 'Prof. Mark Gumah',
-    position: 'Lorem, ipsum dolor sit amet consectetur.',
-    ach: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+    name: 'SohYeong Noh',
+    position: 'Derictor of Art Central Nobi',
+    ach: 'As the main venue for digital space, Noh promotes understanding among technology.',
   },
+
   {
     id: '5',
     img: './assets/images/speaker_05.png',
-    name: 'Yochai BenKler',
-    position: 'Lorem, ipsum dolor sit amet consectetur.',
-    ach: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+    name: 'Lela Tretikov',
+    position: 'Executive Director of the Wikimedia Foundation',
+    ach: 'Lila Tretikov is the Executive Director of the Wikimedia Foundation who aims at developing more people through out the world.',
   },
+
   {
     id: '6',
     img: './assets/images/speaker_06.png',
-    name: 'Yochai BenKler',
-    position: 'Lorem, ipsum dolor sit amet consectetur.',
-    ach: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+    name: 'Ryan Merkley',
+    position: 'CEO of creative commons, ex COO of the moxilla Foundation',
+    ach: 'Ryan had been leading open-source projects at the Mozilla Foundation.',
   },
 ];
 
@@ -66,8 +69,8 @@ window.onload = () => {
 };
 
 // Toggle code
-let togBtn = document.querySelector('.toggle');
-let icon = togBtn.querySelector('.fa-bars');
+const togBtn = document.querySelector('.toggle');
+const icon = togBtn.querySelector('.fa-bars');
 togBtn.onclick = function () {
   if (icon.classList.contains('fa-bars')) {
     icon.classList.replace('fa-bars', 'fa-times');
@@ -83,3 +86,9 @@ toggle.addEventListener('click', () => {
   toggle.classList.toggle('active');
   mobNav.classList.toggle('active');
 });
+
+// Close NavMenu
+document.querySelectorAll('.nav-link').forEach(n => n.addEventListener('click', () => {
+  toggle.classList.remove('active');
+  mobNav.classList.remove('active');
+}));
